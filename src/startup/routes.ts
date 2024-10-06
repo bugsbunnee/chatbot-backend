a2c47929ba6e4f2d54120a115c78d9be635ff1fd
marcel <chukwuma.marcel@quickcheck.ng>
2024-10-06 17:19:10 +0100

    disaling-headers

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -14,8 +14,8 @@ import error from "@/middleware/error";
 
 function registerRoutes(app: Express) {
     // app.use(cors());
-    app.use(compression());
-    app.use(helmet());
+    // app.use(compression());
+    // app.use(helmet());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
