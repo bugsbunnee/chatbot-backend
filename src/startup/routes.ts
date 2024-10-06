d248438dad1e6d088ffd135b4ea93a41f6ba3d47
marcel <chukwuma.marcel@quickcheck.ng>
2024-10-06 17:22:09 +0100

    reenabling-headers

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -13,9 +13,9 @@ import metrics from '@/routes/metrics';
 import error from "@/middleware/error";
 
 function registerRoutes(app: Express) {
-    // app.use(cors());
-    // app.use(compression());
-    // app.use(helmet());
+    app.use(cors());
+    app.use(compression());
+    app.use(helmet());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
